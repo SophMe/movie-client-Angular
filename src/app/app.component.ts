b9db6fd6ecf86cd5b21e3d1c0dead1bc8f875ee7
SophMe <116891325+SophMe@users.noreply.github.com>
2023-07-08 17:11:12 +0200

    set up signup and login view

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+
+import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  // dynamic data
   title = 'movie-client-Angular';
+
+  constructor(public dialog: MatDialog) {}
+    openUserRegistrationDialog(): void {
+      this.dialog.open(UserRegistrationFormComponent, {
+        width: '280px'
+      });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,38 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
+import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
+
+// import { UserRegistrationFormCompnent } from './user-registration-form/user-registration-form.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserRegistrationFormComponent,
+    // UserRegistrationFormCompnent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatButtonModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatDialogModule,
+    MatSnackBarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
